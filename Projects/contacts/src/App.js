71511c75840356454cfc6ac2cfdc91f76a2c62b9
mariyaeggs <mariyawilson@att.net>
2018-03-24 12:58:49 -0700

    Set state in App for list of contacts.

--- Projects/contacts/src/App.js
+++ Projects/contacts/src/App.js
@@ -2,38 +2,40 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import './App.css';
 
-const contacts = [
-  {
-    id: 'mariya',
-    name: 'Mariya Eggensperger',
-    email: 'eggenspergermariya@gmail.com',
-    avatarURL: 'http://localhost:5001/mariya.jpg',
-  },
-  {
-    id: 'scott',
-    name: 'Scott Eggensperger',
-    email: 'scotteggensperger@gmail.com',
-    avatarURL: 'http://localhost:5001/scott.jpg',
-  },
-  {
-    id: 'sofia',
-    name: 'Sofia Eggensperger',
-    email: 'sofiaeggensperger@gmail.com',
-    avatarURL: 'http://localhost:5001/sofia.jpg',
-  },
-  {
-    id: 'oliver',
-    name: 'Oliver Eggensperger',
-    email: 'ollieggensperger@gmail.com',
-    avatarURL: 'http://localhost:5001/oliver.jpg',
-  },
-];
 
 class App extends Component {
+  state = {
+    contacts: [
+    {
+      id: 'mariya',
+      name: 'Mariya Eggensperger',
+      email: 'eggenspergermariya@gmail.com',
+      avatarURL: 'http://localhost:5001/mariya.jpg',
+    },
+    {
+      id: 'scott',
+      name: 'Scott Eggensperger',
+      email: 'scotteggensperger@gmail.com',
+      avatarURL: 'http://localhost:5001/scott.jpg',
+    },
+    {
+      id: 'sofia',
+      name: 'Sofia Eggensperger',
+      email: 'sofiaeggensperger@gmail.com',
+      avatarURL: 'http://localhost:5001/sofia.jpg',
+    },
+    {
+      id: 'oliver',
+      name: 'Oliver Eggensperger',
+      email: 'ollieggensperger@gmail.com',
+      avatarURL: 'http://localhost:5001/oliver.jpg',
+    },
+  ]
+}
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
--- Projects/contacts/src/ListContacts.js
+++ Projects/contacts/src/ListContacts.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className='contact-list'>
-      {this.props.contacts.map((contact) =>
+function ListContacts(props) {
+  return (
+    <ol className='contact-list'>
+      {props.contacts.map((contact) =>
         <li key={contact.id} className='contact-list-item'>
           <div className='contact-avatar' style={{
               backgroundImage: `url(${contact.avatarURL})`
@@ -13,10 +12,12 @@ class ListContacts extends Component {
             <p>{contact.name}</p>
             <p>{contact.email}</p>
           </div>
+          <button className='contact-remove'>
+            Remove
+          </button>
         </li>
-       )}
+      )}
     </ol>
-    )
-  }
+  )
 }
 export default ListContacts;
